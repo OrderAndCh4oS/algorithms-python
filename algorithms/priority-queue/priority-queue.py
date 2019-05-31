ed567526d5132e3c9e68e194cc4a334a5d54a002
Sarcoma <sarcoma@live.co.uk>
2019-05-31 09:02:51 +0100

    Fixed initial insert bugs

--- algorithms/priority-queue/priority-queue.py
+++ algorithms/priority-queue/priority-queue.py
@@ -1,3 +1,6 @@
+import random
+
+
 class PriorityQueue:
     """
     Note: https://algs4.cs.princeton.edu/24pq/
@@ -8,10 +11,11 @@ class PriorityQueue:
 
     def insert(self, a):
         self.priority_queue.append(a)
+        self.n += 1
         if self.is_empty():
             return
         self.swim(self.size())
-        self.n += 1
+
         assert self.is_max_heap(1)
 
     def swim(self, key):
@@ -21,13 +25,10 @@ class PriorityQueue:
 
     def sink(self, key):
         while key * 2 <= self.size():
-            print(self.priority_queue)
             j = key * 2
             if j < self.size() and self.less(j, j + 1):
                 j += 1
             if not self.less(key, j):
-                print('Break')
-                print(self.priority_queue)
                 break
             self.exchange(key, j)
             key = j
@@ -66,7 +67,7 @@ class PriorityQueue:
         return maximum
 
     def is_empty(self):
-        return len(self.priority_queue) == 1
+        return self.n == 0
 
     def size(self):
         return self.n
@@ -78,29 +79,21 @@ class PriorityQueue:
 if __name__ == '__main__':
     pq = PriorityQueue()
 
-    pq.insert(4)
-    pq.insert(2)
-    pq.insert(16)
-    pq.insert(7)
-    pq.insert(11)
-    pq.insert(15)
-    pq.insert(9)
-    pq.insert(14)
-    pq.insert(12)
-    pq.insert(5)
-    pq.insert(3)
-    pq.insert(13)
-    pq.insert(6)
-    pq.insert(8)
-    pq.insert(17)
-    pq.insert(1)
-    pq.insert(10)
+    arr = list(range(18))
+    random.shuffle(arr)
 
-    print('Result')
-    print(pq)
+    for x in arr:
+        pq.insert(x)
+        print(pq)
+
+    sorted_arr = []
+
     for _ in range(17):
         print(pq)
-        print(pq.delete_max())
+        maximum = pq.delete_max()
+        sorted_arr.append(maximum)
         print('Result')
         print(pq)
         print('________')
+
+    print(sorted_arr)
